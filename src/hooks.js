1765a0a84ccc1b57d64f0fda0d9f606337aae277
Filip Jaskólski <f.jaskolski@livechatinc.com>
2019-06-13 13:04:37 +0200

    lazy init for localstorage hook

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const useLocalStorageAndState = (key, defaultValue = "") => {
   const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || defaultValue
+    () => JSON.parse(localStorage.getItem(key)) || defaultValue
   );
 
   useEffect(() => {
